3f66fb701a02b63a380eb6306672ba0d4b72d9c8
lostmart <lostmart@hotmail.com>
2024-11-28 11:09:06 +0100

    new adds

--- src/main/java/com/mooc/luditech/services/implementation/EnterpriseServiceImplementation.java
+++ src/main/java/com/mooc/luditech/services/implementation/EnterpriseServiceImplementation.java
@@ -3,34 +3,28 @@ package com.mooc.luditech.services.implementation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mooc.luditech.dao.EnterpriseRepository;
 import com.mooc.luditech.models.Enterprise;
 import com.mooc.luditech.services.EnterpriseService;
 
 @Service
 public class EnterpriseServiceImplementation implements EnterpriseService {
+	
+	@Autowired
+	private EnterpriseRepository enterpriseRepository;
 
 	@Override
 	public List<Enterprise> getAll() {
-	List<Enterprise> enterprises = new ArrayList<Enterprise>();
+
+		List<Enterprise> enterprises = new ArrayList<Enterprise>();
 		
-		Enterprise enterprise1 = new Enterprise();
-		enterprise1.setId(1);
-		enterprise1.setName("microsoft");
-		enterprise1.setCity("New York ssss");
-		
-		
-		
-		Enterprise enterprise2 = new Enterprise();
-		enterprise2.setId(2);
-		enterprise2.setName("oracle");
-		enterprise2.setCity("Washinton");
-		
-		enterprises.add(enterprise1);
-		enterprises.add(enterprise2);
+		enterpriseRepository.findAll().forEach(enterprises::add);
 		
 		return enterprises;
+
 	}
 
 }
